14ebdcd8baa6d8102aecc6908d3f52d42af5b801
FernandoCR1976 <fernandocorralesm76@gmail.com>
2021-03-18 15:51:15 +0000

    more changes

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,7 +4,12 @@ export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
 			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			<a href="http://www.4geeksacademy.com">4Geeks Academy & Rust Pirate</a>
 		</p>
+		<img
+			src="https://static7.depositphotos.com/1181789/760/i/950/depositphotos_7608928-stock-photo-fire-burning-flaming-metallic-skull.jpg"
+			width="80"
+			height="80"
+		/>
 	</footer>
 );
