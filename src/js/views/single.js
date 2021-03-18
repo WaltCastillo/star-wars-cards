02f4bf2e1246eb97cdbdcd77db2fe5418fee87a8
FernandoCR1976 <fernandocorralesm76@gmail.com>
2021-03-18 01:25:45 +0000

    lorem add

--- src/js/views/single.js
+++ src/js/views/single.js
@@ -26,7 +26,7 @@ export const Single = props => {
 								<h1 className="display-4">{store.single.name}</h1>
 								<p className="lead">
 									{
-										"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+										"Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo"
 									}
 								</p>
 							</div>
